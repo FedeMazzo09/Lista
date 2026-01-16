4fc32e5c007b9e8ebd09a9745d039fae32cf93e0
MAZZOLENI.22138 <mazzoleni.federico.studente@itispaleocapa.it>
2026-01-16 13:49:32 +0100

    Add Lista and Nodo classes with basic linked list operations and a test implementation

--- Test.java
+++ Test.java
@@ -1,10 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-
-        // Creazione lista vuota
         Lista l = new Lista(0, null, null);
 
-        // ACCODAMENTO
         l.accoda("A");
         l.accoda("B");
         l.accoda("C");
@@ -13,39 +10,30 @@ public class Test {
 
         System.out.println("Lista creata con accodamento");
 
-        // LETTURA
-        System.out.println("Lettura indice 0: " + l.lettura(0)); // A
-        System.out.println("Lettura indice 2: " + l.lettura(2)); // C
+        System.out.println("Lettura indice 0: " + l.lettura(0));
+        System.out.println("Lettura indice 2: " + l.lettura(2));
 
-        // RICERCA
         int pos = l.ricerca("B");
         System.out.println("Prima occorrenza di B alla posizione: " + pos);
 
-        // INSERIMENTO
         l.inserimento("X", 2);
         System.out.println("Inserito X in posizione 2");
 
-        // LETTURA dopo inserimento
-        System.out.println("Lettura indice 2: " + l.lettura(2)); // X
+        System.out.println("Lettura indice 2: " + l.lettura(2));
 
-        // VISITA (con cursore)
         l.cursor = l.head;
-        System.out.println("Visita 1: " + l.visita()); // A
-        System.out.println("Visita 2: " + l.visita()); // B
-        System.out.println("Visita 3: " + l.visita()); // X
+        System.out.println("Visita 1: " + l.visita());
+        System.out.println("Visita 2: " + l.visita());
+        System.out.println("Visita 3: " + l.visita());
 
-        // ELIMINAZIONE per indice
         l.eliminazione(1);
         System.out.println("Eliminato elemento in posizione 1");
 
-        // LETTURA dopo eliminazione
         System.out.println("Lettura indice 1: " + l.lettura(1));
 
-        // ELIMINAZIONE COMPLETA
         l.eliminazioneCompleta("B");
         System.out.println("Eliminate tutte le B");
 
-        // STAMPA FINALE usando visita
         System.out.println("Lista finale:");
         l.cursor = l.head;
         String valore;
